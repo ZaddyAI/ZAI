ad71d6881992de0c27d7740c3c5429c0de870dec
Gambhir Poudel <gambhir.kutumba@gmail.com>
2025-05-20 15:29:08 +0545

    Added G

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package zai
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress theme</title>
+</head>
+<body>
+Hello this
+</body>
+</html>
