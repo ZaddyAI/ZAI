29012c4ba85b46cf88c3241fa8e1f82e25c1d509
Gambhir Poudel <gambhir.kutumba@gmail.com>
2025-05-27 18:23:36 +0545

    Fix some issue regarding file name G

--- functions.php
+++ functions.php
@@ -9,6 +9,9 @@
 	define( 'ZAI_DIR_PATH', untrailingslashit( get_template_directory() ) );
 }
 
+if (! defined( 'ZAI_DIR_URI' ) ) {
+	define( 'ZAI_DIR_URI', untrailingslashit( get_template_directory_uri() ) );
+}
 require_once ZAI_DIR_PATH . '/inc/helpers/autoloader.php';
 
 function zai_get_theme_instance() {
@@ -18,19 +21,8 @@ function zai_get_theme_instance() {
 zai_get_theme_instance();
 
 function zai_enqueue_scripts() {
-// Register Styles.
-   wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_stylesheet_directory(). '/style.css'), 'all');
-   wp_register_style('bootstrap-css', get_template_directory_uri(). '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
-// Register Scripts.
-    wp_register_script('main-js', get_template_directory_uri(). '/assets/main.js', [], filemtime(get_template_directory(). '/assets/main.js'), true);
-    wp_register_script('bootstrap-js', get_template_directory_uri(). '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true);
 
-    // enqueue styles
-    wp_enqueue_style('style-css');
-    wp_enqueue_style('bootstrap-css');
-    // enqueue scripts
-    wp_enqueue_script('main-js');
-    wp_enqueue_script('bootstrap-js');
+
 
 }
 add_action( 'wp_enqueue_scripts', 'zai_enqueue_scripts' );
